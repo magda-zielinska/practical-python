9856c593ddd4a1c9faab6c78bfd5977f1aeb010c
Magdalena Zielinska <zielinska.magdalen@gmail.com>
2020-07-16 11:25:20 +0200

    Add solution to the mortgage problem

--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,28 @@
 # mortgage.py
 #
 # Exercise 1.7
+
+mortgage = 500000.0
+interest_rate = 0.05
+monthly_payment = 2684.11
+paid = 0.00
+months = 0
+
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+extra_payment = 1000
+
+while mortgage > 0:
+
+    mortgage = mortgage * (1 + interest_rate / 12) - monthly_payment
+    paid = paid + monthly_payment
+    months = months + 1
+
+    if extra_payment_start_month <= months <= extra_payment_end_month:
+        mortgage = mortgage - 1000.00
+        paid = paid + 1000.00
+
+    print(months, round(paid, 2), round(mortgage, 2))
+
+print(round(paid, 2), months)
+
