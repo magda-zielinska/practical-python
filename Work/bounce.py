994fded9ab8825726ca9e3939e4bb99c33de76ca
Magdalena Zielinska <zielinska.magdalen@gmail.com>
2020-07-16 10:21:24 +0200

    Add solution to the bouncing ball problem

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,17 @@
 # bounce.py
 #
 # Exercise 1.5
+
+""" A rubber ball is dropped from a height of 100 meters and each time it hits the ground,
+it bounces back up to 3/5 the height it fell. Write a program bounce.py
+that prints a table showing the height of the first 10 bounces."""
+
+
+height = 100
+bounces = 1
+
+while bounces <= 10:
+    height *= 3/5
+    print(bounces, round(height, 4))
+    bounces += 1
+
