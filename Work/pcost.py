489ef6b997c8ef4c5a57e83aa712bdc2ea49bcac
Magdalena Zielinska <zielinska.magdalen@gmail.com>
2020-07-17 15:47:19 +0200

    Add command line handler

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,21 +1,26 @@
 # pcost.py
 #
 # Exercise 1.27
+import csv
+import sys
 
 
-def portfolio_cost(filename):
+def portfolio_cost():
+    if len(sys.argv) == 2:
+        filename = sys.argv[1]
+    else:
+        filename = 'Data/portfolio.csv'
+    f = open(filename, 'rt')
+    rows = csv.reader(f)
+    next(rows)
     total_cost = 0.00
-    with open(filename, 'rt') as f:
-        next(f)
-        for line in f:
-            try:
-                line = line.split(',')
-                total_cost += int(line[1]) * float(line[2])
-            except ValueError:
-                pass
+    for line in rows:
+        try:
+            total_cost += int(line[1]) * float(line[2])
+        except ValueError:
+            pass
     return total_cost
 
 
-cost = portfolio_cost('Data/portfolio.csv')
+cost = portfolio_cost()
 print(f'Total cost: ', cost)
-
