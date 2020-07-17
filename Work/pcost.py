0744d92c6407d28d72020c662334e37141305d04
Magdalena Zielinska <zielinska.magdalen@gmail.com>
2020-07-17 15:28:44 +0200

    Add error handling

--- Work/pcost.py
+++ Work/pcost.py
@@ -8,10 +8,14 @@ def portfolio_cost(filename):
     with open(filename, 'rt') as f:
         next(f)
         for line in f:
-            line = line.split(',')
-            total_cost += int(line[1]) * float(line[2])
+            try:
+                line = line.split(',')
+                total_cost += int(line[1]) * float(line[2])
+            except ValueError:
+                pass
     return total_cost
 
 
 cost = portfolio_cost('Data/portfolio.csv')
 print(f'Total cost: ', cost)
+
