93b2fddb6ea889925fa9ab063d6dc3410a93d61e
Magdalena Zielinska <zielinska.magdalen@gmail.com>
2020-07-17 14:33:42 +0200

    Add solution to the portfolio problem

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,13 @@
 # pcost.py
 #
 # Exercise 1.27
+
+total_cost = 0.00
+with open('Data/portfolio.csv', 'rt') as f:
+    next(f)
+    for line in f:
+        line = line.split(',')
+        total_cost += int(line[1]) * float(line[2])
+        #print(line)
+
+print(f'Total cost {total_cost}')
