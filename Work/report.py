5e50f8cb44a71530d42177319932aba9656d1a39
Magdalena Zielinska <zielinska.magdalen@gmail.com>
2020-07-21 12:30:32 +0200

    Update the file to read the new stock prices and calculate the gain/loss

--- Work/report.py
+++ Work/report.py
@@ -4,16 +4,62 @@
 
 import csv
 
-portfolio = []
 
 def read_portfolio(filename):
+    """
+    Read a stock portfolio file into a list of dictionaries with keys
+    name, shares, and price.
+    :param filename: filepath
+    :return: a list of dictionaries
+    """
+    portfolio = []
 
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
-        headers = next(rows)
+        next(rows)
         for row in rows:
-            holding = (row[0], int(row[1]), float(row[2]))
+            holding = {'name': row[0], 'shares': int(row[1]), 'price': float(row[2])}
             portfolio.append(holding)
     return portfolio
 
 
+def read_prices(filename):
+    """
+    Read a csv file with stock prices into a dictionary where the key is the name of the stock
+    and the value is its price
+    :param filename: path to file
+    :return: a dictionary
+    """
+    prices = {}
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        for row in rows:
+            # row = row.split(',')
+            try:
+                prices[row[0]] = float(row[1])
+            except IndexError:
+                pass
+    return prices
+
+
+portfolio = read_portfolio('Data/portfolio.csv')
+prices = read_prices('Data/prices.csv')
+
+print(portfolio)
+print(prices)
+
+# Compute the total value of the stock from the portfolio file
+total_value = 0.0
+for s in portfolio:
+    total_value += s['shares']*s['price']
+
+print('Total cost', total_value)
+
+# Compute the current value of the portfolio based on the new prices
+total_new_value = 0.0
+for s in portfolio:
+    total_new_value += s['shares']*prices[s['name']]
+
+print('Current value', total_new_value)
+print('Gain', total_new_value - total_value)
