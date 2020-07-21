778ff9c0b648e36af187f6e935c985c1c7d85dd5
Magdalena Zielinska <zielinska.magdalen@gmail.com>
2020-07-21 09:03:51 +0200

    Add the solution to the list portfolio problem

--- Work/report.py
+++ Work/report.py
@@ -1,3 +1,19 @@
 # report.py
 #
 # Exercise 2.4
+
+import csv
+
+portfolio = []
+
+def read_portfolio(filename):
+
+    with open(filename, 'rt') as f:
+        rows = csv.reader(f)
+        headers = next(rows)
+        for row in rows:
+            holding = (row[0], int(row[1]), float(row[2]))
+            portfolio.append(holding)
+    return portfolio
+
+
