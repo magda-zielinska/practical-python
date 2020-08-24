586cd1c7b18076f2b5cf8ae0e3e96ab5a048ae81
Magdalena Zielinska <zielinska.magdalen@gmail.com>
2020-08-24 10:47:18 +0200

    Add the usage of the fileparse module

--- Work/pcost.py
+++ Work/pcost.py
@@ -3,33 +3,36 @@
 # Exercise 1.27
 import csv
 import sys
-
+from Work import report
 
 def portfolio_cost():
     if len(sys.argv) == 2:
         filename = sys.argv[1]
     else:
         filename = 'Data/portfolio.csv'
-    with open(filename, 'rt') as f:
-        rows = csv.reader(f)
-        next(rows)
+
+        rows = report.read_portfolio(filename)
         total_cost = 0.00
         for line in rows:
             try:
-                total_cost += int(line[1]) * float(line[2])
+                total_cost += int(line['shares']) * float(line['price'])
             except ValueError:
                 pass
         return total_cost
 
 def missing_portfolio():
 
-    with open('Data/missing.csv') as f:
+    with open('Data/portfoliodate.csv') as f:
         rows = csv.reader(f)
+        headers = next(rows)
         next(rows)
         missing_cost = 0.00
-        for rowno, row in enumerate(rows):
+        for rowno, row in enumerate(rows, start=1):
+            record = dict(zip(headers, row))
             try:
-                missing_cost += int(row[1]) * float(row[2])
+                nshares = int(record['shares'])
+                price = float(record['price'])
+                missing_cost += nshares * price
             except ValueError:
                 print(f'Row {rowno}: Couldn\'t convert: {row}')
     return missing_cost
